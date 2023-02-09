b115cd7b0ac6e4af43750a9234151e59a9edf0ba
ImadRashid <imadrashid789@gmail.com>
2023-02-09 19:36:22 +0500

    feat: added crashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:get/get.dart';
@@ -5,14 +6,19 @@ import 'package:karbanboard/core/restart_app.dart';
 import 'package:karbanboard/locator.dart';
 import 'package:karbanboard/ui/screens/board/kanban_provider.dart';
 import 'package:karbanboard/ui/screens/splash_screen.dart';
-import 'package:provider/provider.dart';
 import 'firebase_options.dart';
+import 'dart:ui';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
   await setupLocator();
   runApp(MyApp());
 }
